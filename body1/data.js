ce66121da1d4ca2777b7abd460bf04e229bb5b56
minzhang190 <minzhang190@gmail.com>
2020-04-13 10:44:09 +0000

    +body1/2

---
+++ body1/data.js
@@ -0,0 +1,94 @@
+var title = "Shēntǐ";
+
+var data = [
+    {
+        "image": "assets/5e943b2b25c505681d0f1dee.png",
+        "sound": "assets/5e943b2b25c505681d0f1dee.mp3",
+        "pinyin": "shēn tǐ",
+        "text": "身体"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430631.png",
+        "sound": "assets/5e8dac932c02d5b61d430631.mp3",
+        "pinyin": "tóu fà",
+        "text": "头发"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063c.png",
+        "sound": "assets/5e8dac932c02d5b61d43063c.mp3",
+        "pinyin": "bó zi",
+        "text": "脖子"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062e.png",
+        "sound": "assets/5e8dac932c02d5b61d43062e.mp3",
+        "pinyin": "tóu",
+        "text": "头"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062f.png",
+        "sound": "assets/5e8dac932c02d5b61d43062f.mp3",
+        "pinyin": "jiān bǎng",
+        "text": "肩膀"
+    },
+    {
+        "image": "assets/5e943b2d25c505681d0f1def.png",
+        "sound": "assets/5e943b2d25c505681d0f1def.mp3",
+        "pinyin": "xiōng pú",
+        "text": "胸脯"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062d.png",
+        "sound": "assets/5e8dac932c02d5b61d43062d.mp3",
+        "pinyin": "shǒu",
+        "text": "手"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430630.png",
+        "sound": "assets/5e8dac932c02d5b61d430630.mp3",
+        "pinyin": "shǒu zhǐ",
+        "text": "手指"
+    },
+    {
+        "image": "assets/5e943b2e25c505681d0f1df0.png",
+        "sound": "assets/5e943b2e25c505681d0f1df0.mp3",
+        "pinyin": "jiǎo",
+        "text": "脚"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063d.png",
+        "sound": "assets/5e8dac932c02d5b61d43063d.mp3",
+        "pinyin": "jiǎo zhǐ",
+        "text": "脚趾"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063b.png",
+        "sound": "assets/5e8dac932c02d5b61d43063b.mp3",
+        "pinyin": "tuǐ",
+        "text": "腿"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430638.png",
+        "sound": "assets/5e8dac932c02d5b61d430638.mp3",
+        "pinyin": "xī gài",
+        "text": "膝盖"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43062c.png",
+        "sound": "assets/5e8dac932c02d5b61d43062c.mp3",
+        "pinyin": "gē bo",
+        "text": "胳膊"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063f.png",
+        "sound": "assets/5e8dac932c02d5b61d43063f.mp3",
+        "pinyin": "gē bo zhǒu",
+        "text": "胳膊肘"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430639.png",
+        "sound": "assets/5e8dac932c02d5b61d430639.mp3",
+        "pinyin": "dù zi",
+        "text": "肚子"
+    }
+];
---
+++ body2/data.js
@@ -0,0 +1,94 @@
+var title = "Shēntǐ";
+
+var data = [
+    {
+        "image": "assets/5e8dac932c02d5b61d43063a.png",
+        "sound": "assets/5e8dac932c02d5b61d43063a.mp3",
+        "pinyin": "yá chǐ",
+        "text": "牙齿"
+    },
+    {
+        "image": "assets/5e943b3125c505681d0f1df1.png",
+        "sound": "assets/5e943b3125c505681d0f1df1.mp3",
+        "pinyin": "zuǐ chún",
+        "text": "嘴唇"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430636.png",
+        "sound": "assets/5e8dac932c02d5b61d430636.mp3",
+        "pinyin": "zuǐ",
+        "text": "嘴"
+    },
+    {
+        "image": "assets/5e943b3225c505681d0f1df2.png",
+        "sound": "assets/5e943b3225c505681d0f1df2.mp3",
+        "pinyin": "xià bā",
+        "text": "下巴"
+    },
+    {
+        "image": "assets/5e943b3225c505681d0f1df3.png",
+        "sound": "assets/5e943b3225c505681d0f1df3.mp3",
+        "pinyin": "qián é",
+        "text": "前额"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430634.png",
+        "sound": "assets/5e8dac932c02d5b61d430634.mp3",
+        "pinyin": "yǎn jīng",
+        "text": "眼睛"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430633.png",
+        "sound": "assets/5e8dac932c02d5b61d430633.mp3",
+        "pinyin": "ěr duō",
+        "text": "耳朵"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430635.png",
+        "sound": "assets/5e8dac932c02d5b61d430635.mp3",
+        "pinyin": "bí zi",
+        "text": "鼻子"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430632.png",
+        "sound": "assets/5e8dac932c02d5b61d430632.mp3",
+        "pinyin": "liǎn",
+        "text": "脸"
+    },
+    {
+        "image": "assets/5e943b3425c505681d0f1df4.png",
+        "sound": "assets/5e943b3425c505681d0f1df4.mp3",
+        "pinyin": "jiǎo huái",
+        "text": "脚踝"
+    },
+    {
+        "image": "assets/5e943b3425c505681d0f1df5.png",
+        "sound": "assets/5e943b3425c505681d0f1df5.mp3",
+        "pinyin": "hóu lóng",
+        "text": "喉咙"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d430637.png",
+        "sound": "assets/5e8dac932c02d5b61d430637.mp3",
+        "pinyin": "méi máo",
+        "text": "眉毛"
+    },
+    {
+        "image": "assets/5e8dac932c02d5b61d43063e.png",
+        "sound": "assets/5e8dac932c02d5b61d43063e.mp3",
+        "pinyin": "hòu bèi",
+        "text": "后背"
+    },
+    {
+        "image": "assets/5e943b3625c505681d0f1df6.png",
+        "sound": "assets/5e943b3625c505681d0f1df6.mp3",
+        "pinyin": "shé tóu",
+        "text": "舌头"
+    },
+    {
+        "image": "assets/5e943b3625c505681d0f1df7.png",
+        "sound": "assets/5e943b3625c505681d0f1df7.mp3",
+        "pinyin": "gǔ tóu",
+        "text": "骨头"
+    }
+];
