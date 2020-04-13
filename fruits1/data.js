aa59ab3e0a956fbc339459d122bb6ff6d0ba0882
minzhang190 <minzhang190@gmail.com>
2020-04-13 06:22:21 +0000

    +fruits1/2

---
+++ fruits1/data.js
@@ -0,0 +1,64 @@
+var title = "Shuǐguǒ (1)";
+
+var data = [
+    {
+        "image": "assets/5e8bf93af6082885ff046aa6.png",
+        "sound": "assets/5e8bf93af6082885ff046aa6.mp3",
+        "pinyin": "píng guǒ",
+        "text": "苹果"
+    },
+    {
+        "image": "assets/5e8bf93bf6082885ff046aa7.png",
+        "sound": "assets/5e8bf93bf6082885ff046aa7.mp3",
+        "pinyin": "lí",
+        "text": "梨"
+    },
+    {
+        "image": "assets/5e8bf93cf6082885ff046aa8.png",
+        "sound": "assets/5e8bf93cf6082885ff046aa8.mp3",
+        "pinyin": "shuǐ guǒ",
+        "text": "水果"
+    },
+    {
+        "image": "assets/5e8bf93cf6082885ff046aa9.png",
+        "sound": "assets/5e8bf93cf6082885ff046aa9.mp3",
+        "pinyin": "xiāng jiāo",
+        "text": "香蕉"
+    },
+    {
+        "image": "assets/5e8bf93df6082885ff046aaa.png",
+        "sound": "assets/5e8bf93df6082885ff046aaa.mp3",
+        "pinyin": "pú tao",
+        "text": "葡萄"
+    },
+    {
+        "image": "assets/5e8bf93ef6082885ff046aab.png",
+        "sound": "assets/5e8bf93ef6082885ff046aab.mp3",
+        "pinyin": "xī guā",
+        "text": "西瓜"
+    },
+    {
+        "image": "assets/5e8bf93ef6082885ff046aac.png",
+        "sound": "assets/5e8bf93ef6082885ff046aac.mp3",
+        "pinyin": "bō luó",
+        "text": "菠萝"
+    },
+    {
+        "image": "assets/5e8bf93ff6082885ff046aad.png",
+        "sound": "assets/5e8bf93ff6082885ff046aad.mp3",
+        "pinyin": "táo zi",
+        "text": "桃子"
+    },
+    {
+        "image": "assets/5e8bf940f6082885ff046aae.png",
+        "sound": "assets/5e8bf940f6082885ff046aae.mp3",
+        "pinyin": "lǐ zi",
+        "text": "李子"
+    },
+    {
+        "image": "assets/5e8bf941f6082885ff046aaf.png",
+        "sound": "assets/5e8bf941f6082885ff046aaf.mp3",
+        "pinyin": "chéng zi",
+        "text": "橙子"
+    }
+];
---
+++ fruits2/data.js
@@ -0,0 +1,65 @@
+var title = "Shuǐguǒ (2)";
+
+var data = [
+    {
+        maxAlternatives: 2,
+        "image": "assets/5e8bf942f6082885ff046ab0.png",
+        "sound": "assets/5e8bf942f6082885ff046ab0.mp3",
+        "pinyin": "shí liu",
+        "text": "石榴"
+    },
+    {
+        "image": "assets/5e8bf942f6082885ff046ab1.png",
+        "sound": "assets/5e8bf942f6082885ff046ab1.mp3",
+        "pinyin": "cǎo méi",
+        "text": "草莓"
+    },
+    {
+        "image": "assets/5e8bf943f6082885ff046ab2.png",
+        "sound": "assets/5e8bf943f6082885ff046ab2.mp3",
+        "pinyin": "máng guǒ",
+        "text": "芒果"
+    },
+    {
+        "image": "assets/5e8bf944f6082885ff046ab3.png",
+        "sound": "assets/5e8bf944f6082885ff046ab3.mp3",
+        "pinyin": "níng méng",
+        "text": "柠檬"
+    },
+    {
+        "image": "assets/5e8bf944f6082885ff046ab4.png",
+        "sound": "assets/5e8bf944f6082885ff046ab4.mp3",
+        "pinyin": "yīng tao",
+        "text": "樱桃"
+    },
+    {
+        "image": "assets/5e8bf945f6082885ff046ab5.png",
+        "sound": "assets/5e8bf945f6082885ff046ab5.mp3",
+        "pinyin": "yē zi",
+        "text": "椰子"
+    },
+    {
+        "image": "assets/5e8bf946f6082885ff046ab6.png",
+        "sound": "assets/5e8bf946f6082885ff046ab6.mp3",
+        "pinyin": "niú yóu guǒ",
+        "text": "牛油果"
+    },
+    {
+        "image": "assets/5e8bf946f6082885ff046ab7.png",
+        "sound": "assets/5e8bf946f6082885ff046ab7.mp3",
+        "pinyin": "hēi méi",
+        "text": "黑莓"
+    },
+    {
+        "image": "assets/5e8bf947f6082885ff046ab8.png",
+        "sound": "assets/5e8bf947f6082885ff046ab8.mp3",
+        "pinyin": "lán méi",
+        "text": "蓝莓"
+    },
+    {
+        "image": "assets/5e8bf948f6082885ff046ab9.png",
+        "sound": "assets/5e8bf948f6082885ff046ab9.mp3",
+        "pinyin": "mí hóu táo",
+        "text": "猕猴桃"
+    }
+];
