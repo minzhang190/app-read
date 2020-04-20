84756feea6e7cdd04f7b015b02522e19a5f9bfe7
minzhang190 <minzhang190@gmail.com>
2020-04-20 03:21:35 +0000

    +animals1/2

---
+++ animals1/data.js
@@ -0,0 +1,52 @@
+var title = "Dòngwu (1)";
+
+var data = [
+    {
+        "image": "assets/5e9cda08037c6b87b73250f1.png",
+        "sound": "assets/5e9cda08037c6b87b73250f1.mp3",
+        "pinyin": "dòng wu",
+        "text": "动物"
+    },
+    {
+        "image": "assets/5e9cda09037c6b87b73250f2.png",
+        "sound": "assets/5e9cda09037c6b87b73250f2.mp3",
+        "pinyin": "niú",
+        "text": "牛"
+    },
+    {
+        "image": "assets/5e9cda09037c6b87b73250f3.png",
+        "sound": "assets/5e9cda09037c6b87b73250f3.mp3",
+        "pinyin": "shān yáng",
+        "text": "山羊"
+    },
+    {
+        "image": "assets/5e9cda09037c6b87b73250f4.png",
+        "sound": "assets/5e9cda09037c6b87b73250f4.mp3",
+        "pinyin": "mián yáng",
+        "text": "绵羊"
+    },
+    {
+        "image": "assets/5e8dab8369c2516911ea4531.png",
+        "sound": "assets/5e8dab8369c2516911ea4531.mp3",
+        "pinyin": "gǒu",
+        "text": "狗"
+    },
+    {
+        "image": "assets/5e8dab8369c2516911ea4530.png",
+        "sound": "assets/5e8dab8369c2516911ea4530.mp3",
+        "pinyin": "māo",
+        "text": "猫"
+    },
+    {
+        "image": "assets/5e9cda0b037c6b87b73250f5.png",
+        "sound": "assets/5e9cda0b037c6b87b73250f5.mp3",
+        "pinyin": "gōng jī",
+        "text": "公鸡"
+    },
+    {
+        "image": "assets/5e9cda0b037c6b87b73250f6.png",
+        "sound": "assets/5e9cda0b037c6b87b73250f6.mp3",
+        "pinyin": "mǔ jī",
+        "text": "母鸡"
+    }
+];
---
+++ animals2/data.js
@@ -0,0 +1,52 @@
+var title = "Dòngwu (2)";
+
+var data = [
+    {
+        "image": "assets/5e9cda0b037c6b87b73250f7.png",
+        "sound": "assets/5e9cda0b037c6b87b73250f7.mp3",
+        "pinyin": "xiǎo jī",
+        "text": "小鸡"
+    },
+    {
+        "image": "assets/5e9cda0c037c6b87b73250f8.png",
+        "sound": "assets/5e9cda0c037c6b87b73250f8.mp3",
+        "pinyin": "huǒ jī",
+        "text": "火鸡"
+    },
+    {
+        "image": "assets/5e8dab8369c2516911ea4532.png",
+        "sound": "assets/5e8dab8369c2516911ea4532.mp3",
+        "pinyin": "yā zi",
+        "text": "鸭子"
+    },
+    {
+        "image": "assets/5e9cda0d037c6b87b73250f9.png",
+        "sound": "assets/5e9cda0d037c6b87b73250f9.mp3",
+        "pinyin": "é",
+        "text": "鹅"
+    },
+    {
+        "image": "assets/5e9cda0d037c6b87b73250fa.png",
+        "sound": "assets/5e9cda0d037c6b87b73250fa.mp3",
+        "pinyin": "mǎ",
+        "text": "马"
+    },
+    {
+        "image": "assets/5e9cda0d037c6b87b73250fb.png",
+        "sound": "assets/5e9cda0d037c6b87b73250fb.mp3",
+        "pinyin": "zhū",
+        "text": "猪"
+    },
+    {
+        "image": "assets/5e9cda0e037c6b87b73250fc.png",
+        "sound": "assets/5e9cda0e037c6b87b73250fc.mp3",
+        "pinyin": "tù zi",
+        "text": "兔子"
+    },
+    {
+        "image": "assets/5e9cda0e037c6b87b73250fd.png",
+        "sound": "assets/5e9cda0e037c6b87b73250fd.mp3",
+        "pinyin": "lǘ",
+        "text": "驴"
+    }
+];
