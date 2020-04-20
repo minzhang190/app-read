2274bd5baaf591722d766715d6c02f9f2f08b95e
minzhang190 <minzhang190@gmail.com>
2020-04-20 00:35:02 +0000

    +20200419g4

---
+++ 20200419g4/data.js
@@ -0,0 +1,154 @@
+var title = "Jìjié, Yuèfèn Hé Xīngqī";
+
+var data = [
+    {
+        "image": "assets/5e9ccac2d50a14ecf823ab95.png",
+        "sound": "assets/5e9ccac2d50a14ecf823ab95.mp3",
+        "pinyin": "jì jié",
+        "text": "季节"
+    },
+    {
+        "image": "assets/5e9ccac3d50a14ecf823ab96.png",
+        "sound": "assets/5e9ccac3d50a14ecf823ab96.mp3",
+        "pinyin": "chūn tiān",
+        "text": "春天"
+    },
+    {
+        "image": "assets/5e9ccac4d50a14ecf823ab97.png",
+        "sound": "assets/5e9ccac4d50a14ecf823ab97.mp3",
+        "pinyin": "xià tiān",
+        "text": "夏天"
+    },
+    {
+        "image": "assets/5e9ccac5d50a14ecf823ab98.png",
+        "sound": "assets/5e9ccac5d50a14ecf823ab98.mp3",
+        "pinyin": "qiū tiān",
+        "text": "秋天"
+    },
+    {
+        "image": "assets/5e9ccac6d50a14ecf823ab99.png",
+        "sound": "assets/5e9ccac6d50a14ecf823ab99.mp3",
+        "pinyin": "dōng tiān",
+        "text": "冬天"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d5.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03d5.mp3",
+        "pinyin": "yī yuè",
+        "text": "一月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d6.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03d6.mp3",
+        "pinyin": "èr yuè",
+        "text": "二月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d7.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03d7.mp3",
+        "pinyin": "sān yuè",
+        "text": "三月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d8.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03d8.mp3",
+        "pinyin": "sì yuè",
+        "text": "四月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03d9.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03d9.mp3",
+        "pinyin": "wǔ yuè",
+        "text": "五月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03da.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03da.mp3",
+        "pinyin": "liù yuè",
+        "text": "六月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03db.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03db.mp3",
+        "pinyin": "qī yuè",
+        "text": "七月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03dc.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03dc.mp3",
+        "pinyin": "bā yuè",
+        "text": "八月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03dd.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03dd.mp3",
+        "pinyin": "jiǔ yuè",
+        "text": "九月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03de.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03de.mp3",
+        "pinyin": "shí yuè",
+        "text": "十月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03df.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03df.mp3",
+        "pinyin": "shí yī yuè",
+        "text": "十一月"
+    },
+    {
+        "image": "assets/5e8dae7caf74119e4f2c03e0.png",
+        "sound": "assets/5e8dae7caf74119e4f2c03e0.mp3",
+        "pinyin": "shí èr yuè",
+        "text": "十二月"
+    },
+    {
+        "image": "assets/5e9cd58217c76e7d6c69f908.png",
+        "sound": "assets/5e9cd58217c76e7d6c69f908.mp3",
+        "pinyin": "xīng qī",
+        "text": "星期"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dbf.png",
+        "sound": "assets/5e8dad1da5b7500807899dbf.mp3",
+        "pinyin": "xīng qī yī",
+        "text": "星期一"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc0.png",
+        "sound": "assets/5e8dad1da5b7500807899dc0.mp3",
+        "pinyin": "xīng qī èr",
+        "text": "星期二"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc1.png",
+        "sound": "assets/5e8dad1da5b7500807899dc1.mp3",
+        "pinyin": "xīng qī sān",
+        "text": "星期三"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc2.png",
+        "sound": "assets/5e8dad1da5b7500807899dc2.mp3",
+        "pinyin": "xīng qī sì",
+        "text": "星期四"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc3.png",
+        "sound": "assets/5e8dad1da5b7500807899dc3.mp3",
+        "pinyin": "xīng qī wǔ",
+        "text": "星期五"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc4.png",
+        "sound": "assets/5e8dad1da5b7500807899dc4.mp3",
+        "pinyin": "xīng qī liù",
+        "text": "星期六"
+    },
+    {
+        "image": "assets/5e8dad1da5b7500807899dc5.png",
+        "sound": "assets/5e8dad1da5b7500807899dc5.mp3",
+        "pinyin": "xīng qī rì",
+        "text": "星期日"
+    }
+];
