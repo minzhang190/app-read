25f2c216ba08dc3eb2c8e3ad587abcf5480e1289
minzhang190 <minzhang190@gmail.com>
2020-04-25 03:40:19 +0000

    +weather1/2

---
+++ weather1/data.js
@@ -0,0 +1,76 @@
+var title = "Tiānqì (1)";
+
+var data = [
+    {
+        "image": "assets/5e9cd3b0c9c8767483d7747a.png",
+        "sound": "assets/5e9cd3b0c9c8767483d7747a.mp3",
+        "pinyin": "tiān qì",
+        "text": "天气"
+    },
+    {
+        "image": "assets/5e9cd3b1c9c8767483d7747c.png",
+        "sound": "assets/5e9cd3b1c9c8767483d7747c.mp3",
+        "pinyin": "tài yáng",
+        "text": "太阳"
+    },
+    {
+        "image": "assets/5e9cd3b3c9c8767483d7747e.png",
+        "sound": "assets/5e9cd3b3c9c8767483d7747e.mp3",
+        "pinyin": "qíng lǎng",
+        "text": "晴朗"
+    },
+    {
+        "image": "assets/5e9cd3b3c9c8767483d7747f.png",
+        "sound": "assets/5e9cd3b3c9c8767483d7747f.mp3",
+        "pinyin": "yún",
+        "text": "云"
+    },
+    {
+        "image": "assets/5e9cd3b5c9c8767483d77480.png",
+        "sound": "assets/5e9cd3b5c9c8767483d77480.mp3",
+        "pinyin": "fēng",
+        "text": "风"
+    },
+    {
+        "image": "assets/5e9cd3b6c9c8767483d77481.png",
+        "sound": "assets/5e9cd3b6c9c8767483d77481.mp3",
+        "pinyin": "yǔ",
+        "text": "雨"
+    },
+    {
+        "image": "assets/5e9cd3b7c9c8767483d77482.png",
+        "sound": "assets/5e9cd3b7c9c8767483d77482.mp3",
+        "pinyin": "xuě",
+        "text": "雪"
+    },
+    {
+        "image": "assets/5e9cd3b9c9c8767483d77483.png",
+        "sound": "assets/5e9cd3b9c9c8767483d77483.mp3",
+        "pinyin": "dǎ léi",
+        "text": "打雷"
+    },
+    {
+        "image": "assets/5e9cd3bac9c8767483d77484.png",
+        "sound": "assets/5e9cd3bac9c8767483d77484.mp3",
+        "pinyin": "shǎn diàn",
+        "text": "闪电"
+    },
+    {
+        "image": "assets/5e9cd3bbc9c8767483d77485.png",
+        "sound": "assets/5e9cd3bbc9c8767483d77485.mp3",
+        "pinyin": "léi zhèn yǔ",
+        "text": "雷阵雨"
+    },
+    {
+        "image": "assets/5e9cd3bcc9c8767483d77486.png",
+        "sound": "assets/5e9cd3bcc9c8767483d77486.mp3",
+        "pinyin": "qǐ wù",
+        "text": "起雾"
+    },
+    {
+        "image": "assets/5e9cd3bcc9c8767483d77487.png",
+        "sound": "assets/5e9cd3bcc9c8767483d77487.mp3",
+        "pinyin": "cǎi hóng",
+        "text": "彩虹"
+    }
+];
---
+++ weather2/data.js
@@ -0,0 +1,76 @@
+var title = "Tiānqì (2)";
+
+var data = [
+    {
+        "image": "assets/5e9cd3b0c9c8767483d7747b.png",
+        "sound": "assets/5e9cd3b0c9c8767483d7747b.mp3",
+        "pinyin": "tiān qì yù bào",
+        "text": "天气预报"
+    },
+    {
+        "image": "assets/5e9cd3b2c9c8767483d7747d.png",
+        "sound": "assets/5e9cd3b2c9c8767483d7747d.mp3",
+        "pinyin": "tài yáng hěn dà",
+        "text": "太阳很大"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f497.png",
+        "sound": "assets/5e8db08a26af9d56f020f497.mp3",
+        "pinyin": "duō yún",
+        "text": "多云"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f494.png",
+        "sound": "assets/5e8db08a26af9d56f020f494.mp3",
+        "pinyin": "guā fēng",
+        "text": "刮风"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f495.png",
+        "sound": "assets/5e8db08a26af9d56f020f495.mp3",
+        "pinyin": "xià yǔ",
+        "text": "下雨"
+    },
+    {
+        "image": "assets/5e8db08a26af9d56f020f496.png",
+        "sound": "assets/5e8db08a26af9d56f020f496.mp3",
+        "pinyin": "xià xuě",
+        "text": "下雪"
+    },
+    {
+        "image": "assets/5e9cd3bdc9c8767483d77488.png",
+        "sound": "assets/5e9cd3bdc9c8767483d77488.mp3",
+        "pinyin": "hěn rè",
+        "text": "很热"
+    },
+    {
+        "image": "assets/5e9cd3bec9c8767483d77489.png",
+        "sound": "assets/5e9cd3bec9c8767483d77489.mp3",
+        "pinyin": "hěn lěng",
+        "text": "很冷"
+    },
+    {
+        "image": "assets/5e9cd3bfc9c8767483d7748a.png",
+        "sound": "assets/5e9cd3bfc9c8767483d7748a.mp3",
+        "pinyin": "nuǎn huo",
+        "text": "暖和"
+    },
+    {
+        "image": "assets/5e9cd3c0c9c8767483d7748b.png",
+        "sound": "assets/5e9cd3c0c9c8767483d7748b.mp3",
+        "pinyin": "liáng kuài",
+        "text": "凉快"
+    },
+    {
+        "image": "assets/5e9cd3c1c9c8767483d7748c.png",
+        "sound": "assets/5e9cd3c1c9c8767483d7748c.mp3",
+        "pinyin": "cháo shī",
+        "text": "潮湿"
+    },
+    {
+        "image": "assets/5e9cd3c2c9c8767483d7748d.png",
+        "sound": "assets/5e9cd3c2c9c8767483d7748d.mp3",
+        "pinyin": "lóng juǎn fēng",
+        "text": "龙卷风"
+    }
+];
