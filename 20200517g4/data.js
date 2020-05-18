e2323565adbe00a7ac9e906f1f971166d6f6d23f
minzhang190 <minzhang190@gmail.com>
2020-05-18 04:02:17 +0000

    +20200517g4

---
+++ 20200517g4/data.js
@@ -0,0 +1,70 @@
+var title = "Jīntiān tiānqì zěnme yàng?";
+
+var data = [
+    {
+        "image": "assets/5e9cd75dfb76c53278b7ea5c.png",
+        "sound": "assets/5e9cd75dfb76c53278b7ea5c.mp3",
+        "pinyin": "zěn me yàng",
+        "text": "怎么样"
+    },
+    {
+        "image": "assets/5e9cd75dfb76c53278b7ea5d.png",
+        "sound": "assets/5e9cd75dfb76c53278b7ea5d.mp3",
+        "pinyin": "nǎ gè",
+        "text": "哪个"
+    },
+    {
+        "image": "assets/5e9cd75efb76c53278b7ea5e.png",
+        "sound": "assets/5e9cd75efb76c53278b7ea5e.mp3",
+        "pinyin": "shén me",
+        "text": "什么"
+    },
+    {
+        "image": "assets/5e9cd75ffb76c53278b7ea5f.png",
+        "sound": "assets/5e9cd75ffb76c53278b7ea5f.mp3",
+        "pinyin": "shì",
+        "text": "是"
+    },
+    {
+        "image": "assets/5e9cd75ffb76c53278b7ea60.png",
+        "sound": "assets/5e9cd75ffb76c53278b7ea60.mp3",
+        "pinyin": "hěn hǎo",
+        "text": "很好"
+    },
+    {
+        "image": "assets/5e9cd760fb76c53278b7ea61.png",
+        "sound": "assets/5e9cd760fb76c53278b7ea61.mp3",
+        "pinyin": "bù hǎo",
+        "text": "不好"
+    },
+    {
+        "image": "assets/5e9cd58117c76e7d6c69f905.png",
+        "sound": "assets/5e9cd58117c76e7d6c69f905.mp3",
+        "pinyin": "jīn tiān",
+        "text": "今天"
+    },
+    {
+        "image": "assets/5e9cd58217c76e7d6c69f907.png",
+        "sound": "assets/5e9cd58217c76e7d6c69f907.mp3",
+        "pinyin": "zuó tiān",
+        "text": "昨天"
+    },
+    {
+        "image": "assets/5e9cd58117c76e7d6c69f906.png",
+        "sound": "assets/5e9cd58117c76e7d6c69f906.mp3",
+        "pinyin": "míng tiān",
+        "text": "明天"
+    },
+    {
+        "image": "assets/5ea3b71497e2a3b784762d36.png",
+        "sound": "assets/5ea3b71497e2a3b784762d36.mp3",
+        "pinyin": "qián tiān",
+        "text": "前天"
+    },
+    {
+        "image": "assets/5ea3b71497e2a3b784762d37.png",
+        "sound": "assets/5ea3b71497e2a3b784762d37.mp3",
+        "pinyin": "hòu tiān",
+        "text": "后天"
+    }
+];
